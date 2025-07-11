3281074262fc48a0bcde3c5d13adec1940ef8b08
[KirstiH] <[kirsti.haro@gmail.com]>
2025-07-11 15:18:45 +0300

    10.12

--- part3/rate-repository-app/src/utils/apolloClient.js
+++ part3/rate-repository-app/src/utils/apolloClient.js
@@ -1,9 +1,11 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
+import Constants from 'expo-constants';
 
 
 const createApolloClient = () => {
   return new ApolloClient({
-    uri: 'http://192.168.100.24:4000/graphql',
+    // uri: 'http://192.168.100.24:4000/graphql',
+    uri: Constants.expoConfig.extra.uri,
     cache: new InMemoryCache(),
   });
 };
